71f2670432fa69c3f547d6024fd02dd3184b07f6
manjulprajapati16-web <manjulprajapati16may@gmail.com>
2026-05-01 21:04:22 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "name" {
+    name = "personname"
+    location = "personlocation"
+  
+}
+resource "azurerm_resource_group" "name1" {
+    name = "personname1"
+    location = "personlocation1"
+  
+}
